088df7f8c9a4384cacba9482753e11b09dbf56f1
Sahil Sinha <sahilsinha.dar@gmail.com>
2025-09-29 16:18:05 +0530

    BZ-44388: feat: increase support for app endpoints
    - extended env based app endpoints

--- blaze/src/main/java/in/breeze/blaze/BlazeUtils.kt
+++ blaze/src/main/java/in/breeze/blaze/BlazeUtils.kt
@@ -4,28 +4,31 @@ import android.net.Uri
 import org.json.JSONObject
 
 fun safeParseJson(jsonString: String): JSONObject {
-  return try {
-    JSONObject(jsonString)
-  } catch (e: Exception) {
-    JSONObject()
-  }
+    return try {
+        JSONObject(jsonString)
+    } catch (e: Exception) {
+        JSONObject()
+    }
 }
 
 fun getBaseUrl(payload: JSONObject): String {
-  val environment = payload.optJSONObject("payload")?.optString("environment") ?: "release"
-  return if (environment == "beta") {
-    "https://app.beta.breeze.in"
-  } else {
-    "https://app.breeze.in"
-  }
+    val environment = payload.optJSONObject("payload")?.optString("environment") ?: "release"
+    return if (environment == "smbBeta") {
+        "https://app.beta.breezesdk.store"
+    } else if (environment == "smbRelease") {
+        "https://app.breezesdk.store"
+    } else if (environment == "beta") {
+        "https://app.beta.breeze.in"
+    } else {
+        "https://app.breeze.in"
+    }
 }
 
 fun isUPIIntentUri(uri: Uri): Boolean {
     val scheme = uri.scheme?.lowercase() ?: return false
     val queryParams = uri.queryParameterNames.associateBy(
         keySelector = { name -> name.lowercase() },
-        valueTransform = { name -> uri.getQueryParameter(name).orEmpty() }
-    )
+        valueTransform = { name -> uri.getQueryParameter(name).orEmpty() })
 
     val hasPayeeAddress = queryParams["pa"]?.isNotBlank() == true
     val hasPayeeName = queryParams["pn"]?.isNotBlank() == true
