03233feb1e252dfc45b24209ba200c1091cf3208
Sahil Sinha <sahilsinha.dar@gmail.com>
2026-08-11 09:34:55 +0530

    feat: introducing dynamic service URL configuration
    - sdk now loads configuration to read service & environment based url

--- blaze/src/main/java/in/breeze/blaze/BlazeConstants.kt
+++ blaze/src/main/java/in/breeze/blaze/BlazeConstants.kt
@@ -2,7 +2,13 @@ package `in`.breeze.blaze
 
 internal object BlazeConstants {
     val UPI_SCHEMES = setOf(
-        "upi", "phonepe", "tez", "gpay", "paytm", "paytmmp", "bhim", 
+        "upi", "phonepe", "tez", "gpay", "paytm", "paytmmp", "bhim",
         "amazonpay", "mobikwik", "freecharge", "credpay"
     )
+
+    const val SHARED_PREF_NAME = "BlazeSharedPref"
+    const val SDK_CONFIG_PREF_NAME = "BlazeSdkConfigPref"
+    const val SDK_CONFIG_URL = "https://sdk.breeze.in/core/sdk.json"
+    const val SDK_CONFIG_KEY = "blazeSdkConfig"
+    const val SDK_CONFIG_TIMEOUT_MS = 3000
 }
